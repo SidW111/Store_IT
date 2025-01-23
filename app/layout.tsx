8f2a8157c4be0fcb07cb7681980883dfad7d5489
SidW111 <111293278+SidW111@users.noreply.github.com>
2025-01-24 00:21:22 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--fonts-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Store It",
+  description: "The solution to save your data",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${poppins.variable} font-poppins antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ lib/appwrite/index.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { cookies } from "next/headers";
+import path from "path";
+
+// Creates a client for an authenticated user session
+export const createSessionClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId);
+
+  // Retrieve session from cookies
+  const session = (await cookies()).get("appwrite-session");
+
+  // Ensure session exists and handle error if not
+  if (!session || !session.value) throw new Error("No session found.");
+
+  // Set the session token for the client
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+
+// Creates a client for an admin user (requires API key)
+export const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secretKey);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+};
+
+// Handling cookie session issues, setting SameSite=None for cross-device compatibility
